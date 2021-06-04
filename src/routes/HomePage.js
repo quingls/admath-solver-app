0e5fbd09f9f3210a0c17ed6296ab12500a77094d
quingls <qlsabado@up.edu.ph>
2021-06-04 17:46:47 +0800

    create a admath-solver app

---
+++ src/routes/HomePage.js
@@ -0,0 +1,73 @@
+import React from "react";
+import './HomePage.css';
+import { useHistory } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Home(){
+    let history = useHistory();
+
+    return (
+        <div className="Home">
+            <Container className="container">
+                <Row>
+                    <Col>
+                    <button className="button"
+                        onClick={() => {
+                            history.push("/carnot-efficiency");
+                        }}
+                    >
+                        Carnot Efficiency Calculator
+                    </button>
+                    <button className="button"
+                        onClick={() => {
+                            history.push("/water-flow-rate");
+                        }}
+                    >
+                        Water Flow Rate Calculator
+                    </button>
+                    </Col>
+                    <Col>
+                    <button className="button"
+                        onClick={() => {
+                            history.push("/michaelis-menten-equation");
+                        }}
+                    >
+                        Michaelis-Menten Equation Calculator
+                    </button>
+                    <button className="button"
+                        onClick={() => {
+                            history.push("/mole");
+                        }}
+                    >
+                        Mole Calculator
+                    </button>
+                    </Col>
+                    <Col>
+                    <button className="button"
+                        onClick={() => {
+                            history.push("/mach-number");
+                        }}
+                    >
+                        Mach Number Calculator
+                    </button>
+                    <button className="button"
+                        onClick={() => {
+                            history.push("compressibility-factor");
+                        }}
+                    >
+                        Compressibility Factor Calculator
+                    </button>
+                    </Col>
+                </Row>
+                
+            </Container>
+            <Container className="about">
+            Ariel L. Magpantay Jr.
+            Mariamne Gayle G. Jordan 
+            BS ChE -Batangas State University
+            </Container>
+        </div>
+    );
+}
+
+export default Home;
